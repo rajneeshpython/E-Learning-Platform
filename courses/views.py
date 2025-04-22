cc5b3be95a8fce5235339369a8b22da65f04f561
Rajneesh Kumar <RajneeshPondy@users.noreply.github.com>
2025-04-22 16:15:54 +0530

    Creating a student registration view and Enrolling in courses

--- courses/views.py
+++ courses/views.py
@@ -13,6 +13,7 @@ from django.views.generic.base import TemplateResponseMixin, View
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.views.generic.list import ListView
 
+from students.forms import CourseEnrollForm
 from .forms import ModuleFormSet
 from .models import Content, Course, Module, Subject
 
@@ -209,3 +210,10 @@ class CourseListView(TemplateResponseMixin, View):
 class CourseDetailView(DetailView):
     model = Course
     template_name = 'courses/course/detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['enroll_form'] = CourseEnrollForm(
+            initial={'course': self.object}
+        )
+        return context
